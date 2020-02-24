39c56644f9971c18eae8da0ba7a15a032b8676e2
Yuji.KAJIYAMA <>
2020-02-24 20:10:50 +0900

    browserSyncを追加

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -12,4 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync({
+        files: [
+            'resources/views/**/*.blade.php',
+            'public/**/*.*'
+        ],
+        proxy: {
+            target: '127.0.0.1:10080'
+        }
+    });
